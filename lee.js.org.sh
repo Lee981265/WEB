b556d5b6d03b1ccc3a17d96747303e45517ac2ca
若尘 <2244057555@qq.com>
2019-10-07 18:32:00 +0800

    666

---
+++ lee.js.org.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+hexo clean
+
+hexo g
+
+# cd 到构建输出的目录下
+cd public
+
+# 部署到自定义域名，没有自定义域名可注释掉
+#echo 'lee.js.org' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+# 部署到 https://<USERNAME>.github.io
+git push -f https://github.com/Lee981265/Lee981265.github.io.git master
+
+cd -
